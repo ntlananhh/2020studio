4594522eb0ae8e73fd1109b9901f08834191523c
lananh <lananh.nguyen@nfq.asia>
2022-09-18 16:50:31 +0700

    Implementing header with sub menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 function Header() {
+    const [show, setShow] = useState(false);
     return (
-        <div className="header">
-            <nav className="navbar sticky-top navbar-expand-lg">
+        <div className="header sticky-top">
+            <nav className="navbar  navbar-expand-lg">
                 <div className="container">
-                    <a className="navbar-brand" href="https://www.wildbaerheule.ch/">2020Studio.</a>
+                    <Link className="navbar-brand" to="/">2020Studio.</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i className="fas fa-bars"></i>
                     </button>
@@ -15,6 +16,30 @@ function Header() {
                             <li className="nav-item active">
                                 <Link className="nav-link" to="/">Home</Link>
                             </li>
+                            <li className="nav-item" onClick={()=> setShow(!show)} >
+                                <span className="nav-link">
+                                    <span className="project-link">Projects</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" bi bi-chevron-down" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/> </svg>                                </span>
+                            </li>
+                        
+                            <div 
+                                className="sub-nav sub-nav-mobile" 
+                                style={ { transform: `scaleY(${show ? 1 : 0})`, height:  `${show ? 280 : 0}px` } }>
+                                <ul className="list-group">
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/housing">Housing</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/public-work">Public work</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/religious-work">Religious work</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/interior">Interior</Link>
+                                    </li>           
+                                </ul>
+                            </div>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/about-us">About</Link>
                             </li>
@@ -22,17 +47,33 @@ function Header() {
                                 <Link className="nav-link" to="/contact">Contact</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/contact">Services</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact">Other</Link>
+                                <Link className="nav-link" to="/other">Other</Link>
                             </li>
                         </ul>
                     </div>
+                    
                 </div>
             </nav>
+            <div 
+                className="sub-nav d-none d-md-block" 
+                style={ { transform: `scaleY(${show ? 1 : 0})`, height:  `${show ? 60 : 0}px` } }>
+                <ul className="list-group">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/housing">Housing</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/public-work">Public work</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/religious-work">Religious work</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/interior">Interior</Link>
+                    </li>           
+                </ul>
+            </div>
         </div>
-                    );
+    );
 }
 
 export default Header;
