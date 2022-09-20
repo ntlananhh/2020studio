e851dea0477c378b54c4424c36c216213ec36663
lananh <lananh.nguyen@nfq.asia>
2022-09-20 07:20:26 +0700

    Implement project detail page

--- src/components/project-detail/index.js
+++ src/components/project-detail/index.js
@@ -1,31 +1,73 @@
 import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Navigate } from 'react-router-dom'
 
 const ProjectDetail = () => {
   let name = useParams().name;
-
+  const projectList = [
+    {
+      "id": 1,
+      "name": "Project 1",
+      "countDetail": 5,
+      "category": 'public-work'
+    },
+    {
+      "id": 2,
+      "name": "Project 2",
+      "countDetail": 5,
+      "category": 'housing'
+    },
+    {
+      "id": 3,
+      "name": "Project 3",
+      "countDetail": 5,
+      "category": 'religious-work'
+    },
+    {
+      "id": 4,
+      "name": "Project 4",
+      "countDetail": 5,
+      "category": 'housing'
+    },
+    {
+      "id": 5,
+      "name": "Project 5",
+      "countDetail": 5,
+      "category": 'interior'
+    }
+  ]
+  const project = projectList.filter((project => project.name.replace(/\s/g, '-') === name)) 
+ if(project.length){
+  var rows = [], i = 0, len = project[0].countDetail;
+  while (++i <= len) rows.push(i);
   return (
-    <div>
-        <Splide aria-label="My Favorite Images" autoWidth={true} type="loop"
-          options={{
-            type: 'loop',
-            height: '450px',
-            autoWidth: true,
-        }}>
-      {/* {(()=>{
-         
-        for( let i=1; i<=countDetail; i++){
-          return
-          <SplideSlide key={i}>
-            <img className="test" src={`./assets/project-detail/${name} ${i}.jpg`} alt="" />
-          </SplideSlide>
+    <>
+      <Splide aria-label="Project detail" id="hi"
+        autoWidth={true} type="loop"
+        options={{
+          type: 'loop',
+          height: '450px',
+          autoWidth: true,
+        }}
+      >
+        {
+          rows.map((row) => {
+            return <SplideSlide key={row}>
+              <div className="img-hover-zoom">
+                <img className="test" src={'/assets/project-detail/' + project[0].name.replace(/\s/g, '-').toLowerCase() + '-' + row + '.jpg'} alt="" />
+              </div>
+            </SplideSlide>
+          })
         }
-      })
-      } */}
-    </Splide>
-    </div>
+      </Splide>
+    </>
   )
+ }
+ else return <Navigate to='/' replace />
+
+
+
 }
 
 export default ProjectDetail
--- src/components/project-list/index.js
+++ src/components/project-list/index.js
@@ -1,68 +1,92 @@
 import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom'
 
-function ProjectList({category}) {
+function ProjectList({ category }) {
+  const categoryList = [
+    {
+      id: 1,
+      name: 'housing'
+    },
+    {
+      id: 2,
+      name: 'public-work'
+    },
+    {
+      id: 3,
+      name: 'religious-work'
+    },
+    {
+      id: 4,
+      name: 'interior'
+    },
+  ]
 
-  const projectList=[
+  const projectList = [
     {
       "id": 1,
       "name": "Project 1",
-      "countDetail": 10,
+      "countDetail": 5,
       "category": 'public-work'
     },
     {
       "id": 2,
-      "name": "Project 1",
-      "countDetail": 10,
+      "name": "Project 2",
+      "countDetail": 5,
       "category": 'housing'
     },
     {
       "id": 3,
-      "name": "Project 1",
-      "countDetail": 10,
+      "name": "Project 3",
+      "countDetail": 5,
       "category": 'religious-work'
     },
     {
       "id": 4,
-      "name": "Project 1",
-      "countDetail": 10,
+      "name": "Project 4",
+      "countDetail": 5,
       "category": 'housing'
     },
     {
       "id": 5,
-      "name": "Project 1",
-      "countDetail": 10,
+      "name": "Project 5",
+      "countDetail": 5,
       "category": 'interior'
     }
   ]
-  const list= category == '' ? projectList : projectList.filter((project => project.category== category));
-  return (
-    <Splide aria-label="My Favorite Images" autoWidth={true} type="loop"
-      options={{
-        type: 'loop',
-        height: '450px',
-        autoWidth: true,
-      }}>
-      {list.length ? list.map((item, index) =>
-        <SplideSlide key={index}>
-          <Link to={{ 
-                    pathname: `/project/${item.name.replace(/\s/g, '-') }`, 
-                    query: {
-                      test: `${item.countDetail}` ,
-                    } 
-          }}>
-            <div className="img-hover-zoom">
-              <img className="test" src={'./assets/project-thumbnail/p' + item.id + '.jpg'} alt="" />
-            </div>
+  const list = category === 'all' ? projectList : projectList.filter((project => project.category === category));
+  if ((category !== 'all' && categoryList.find(cate => cate.name === category) !== 'undefined') || category === 'all') {
+    return (
+      <div className=''>
+      <Splide aria-label="My Favorite Images" autoWidth={true} type={list.length >= 2 ? 'loop' : ''}
+        options={{
+          type: 'loop',
+          height: '450px',
+          autoWidth: list.length >= 2 ? true : false,
+          arrows: list.length >= 2 ? true : false,
+          pagination: false
+        }}>
+        {list.length ? list.map((item, index) =>
+          <SplideSlide key={index}>
+            <Link to={{
+              pathname: `/project/${item.name.replace(/\s/g, '-')}`,
+              query: {
+                test: `${item.countDetail}`,
+              }
+            }}>
+              <div className="img-hover-zoom">
+                <img className="test" src={'./assets/project-thumbnail/p' + item.id + '.jpg'} alt="" />
+              </div>
             </Link>
-        </SplideSlide>
-      )
-    :
-    <>empty</>} 
-    </Splide>
-
-  );
+          </SplideSlide>
+        )
+          :
+          <Navigate to='/' replace />}
+      </Splide>
+      </div>
+    )
+  }
+  else <Navigate to='/' replace />
 }
 
 export default ProjectList;
