0b21144fd56d8bd7bc4a9b48e21ec4d7b2898a18
Lan Anh <nguyenthilananh0606@gmail.com>
2024-06-17 18:01:10 +0700

    Update 17/06/24

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
         <div className= "footer">
             <div className='social-icon-group d-flex justify-content-center'>
                 <span className="social-icon">
-                    <a href='https://getbootstrap.com/docs/4.0/utilities/flex/'><i className="fab fa-facebook-f"></i></a>
+                    <a href='https://www.facebook.com/2O2Ostudio/'><i className="fab fa-facebook-f"></i></a>
                 </span>
                 <span className="social-icon">
-                    <a href='https://getbootstrap.com/docs/4.0/utilities/flex/'><i className="fab fa-linkedin-in"></i></a>
+                    <a href='https://www.facebook.com/2O2Ostudio/'><i className="fab fa-linkedin-in"></i></a>
                 </span>
                 <span className="social-icon">
-                    <a href='https://getbootstrap.com/docs/4.0/utilities/flex/'><i className="fab fa-twitter"></i></a>
+                    <a href='https://www.instagram.com/2020_studio_saigon'><i className="fab fa-instagram"></i></a>
                 </span>
             </div>
             <div className='copy-right d-flex justify-content-center mt-2'>
